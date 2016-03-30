7f9a99e95e4124f8c0d6dd0601f1eb4fb56911fc
hectorgarcia83 <hectorgarcia83@gmail.com>
2016-03-29 22:35:59 -0700

    final commit

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
 	
 		<script src="assets/foundation/js/vendor/jquery.min.js"></script>
 		<script src="assets/bootstrap/js/bootstrap.js"></script>
-		<script src="assets/bootstrap/js/jquery.carousel.fullscreen.js"></script>
+		<!--<script src="assets/bootstrap/js/jquery.carousel.fullscreen.js"></script>-->
 		<script src="assets/jqueryui/jquery-ui.js" type="text/javascript"></script>
 		<script src="assets/jquery.mousewheel.min.js" type="text/javascript"></script>
 		<script src="assets/jquery.kinetic.min.js" type="text/javascript"></script>
@@ -117,3 +117,13 @@
 		<script src="js/main.js"></script>
 	</body>
 </html>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-32840860-17', 'auto');
+  ga('send', 'pageview');
+
+</script>
